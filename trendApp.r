a223544c1bafc37cf2be1d896ffa9cb907e69507
kenneth0321 <53888790+kenneth0321@users.noreply.github.com>
2021-10-07 19:26:10 -0500

    Add files via upload
    set period_all to global

--- trendApp.r
+++ trendApp.r
@@ -13,11 +13,13 @@ agg_table <- read_excel("PL_trend_sample_data.xlsx") %>%
 agg_table$claim_freq <- agg_table$ult_claim_count/agg_table$on_level_premium*1000 
 exponetial.modelF<-lm( log(claim_freq) ~ acc_year, data = agg_table)
 
+period_all =  data.frame(acc_year = c(agg_table$acc_year, 2020,2021))
+
 # Define UI for slider demo app ----
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("PL trend - D&O (US+Intl) (AY PY Combined)"),
+  titlePanel("PL Trend - D&O (US+Intl) (AY PY Combined)"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -70,21 +72,22 @@ server <- function(input, output) {
    input$option_2
    input$option_3
    
-   all_year =  data.frame(acc_year = c(agg_table$acc_year, 2020,2021))
+  
    
-   fitted.allYear <- exp(predict(exponetial.modelF, all_year, type = "response"))
-   exp.model.df <- data.frame(x = all_year,
+   fitted.allYear <- exp(predict(exponetial.modelF, period_all, type = "response"))
+   exp.model.df <- data.frame(x = period_all,
                               all_year = fitted.allYear)
    
    
    #no need to plot so many different curves, save each simulated freq as an additional column to the agg_table, then plot by columns
    #user input period 1 curve fitting
+   
    period_1 <- data.frame(acc_year = c(seq(min(input$option_1),max(input$option_1),1)))
    agg_table_1 <- agg_table %>%
      filter(acc_year %in% period_1$acc_year)
    
    exponetial.modelF1<-lm( log(claim_freq) ~ acc_year, data = agg_table_1)
-   fitted.p1 <- exp(predict(exponetial.modelF1, newdata = all_year, type = "response"))
+   fitted.p1 <- exp(predict(exponetial.modelF1, newdata = period_all, type = "response"))
    
    
    #user input period 2 curve fitting
@@ -93,7 +96,7 @@ server <- function(input, output) {
      filter(acc_year %in% period_2$acc_year)
    
    exponetial.modelF2<-lm( log(claim_freq) ~ acc_year, data = agg_table_2)
-   fitted.p2 <- exp(predict(exponetial.modelF2, newdata = all_year, type = "response"))
+   fitted.p2 <- exp(predict(exponetial.modelF2, newdata = period_all, type = "response"))
    
    
    #user input period 3 curve fitting
@@ -102,7 +105,7 @@ server <- function(input, output) {
      filter(acc_year %in% period_3$acc_year)
    
    exponetial.modelF3<-lm( log(claim_freq) ~ acc_year, data = agg_table_3)
-   fitted.p3 <- exp(predict(exponetial.modelF3, newdata = all_year, type = "response"))
+   fitted.p3 <- exp(predict(exponetial.modelF3, newdata = period_all, type = "response"))
    
    #append to a single table
    exp.model.df <- exp.model.df %>% 
@@ -125,7 +128,7 @@ server <- function(input, output) {
       
       geom_line(data = fitted_models(), aes(x = acc_year , y = modeled_value, colour = model_option), size=2, linetype=1)+
       
-      scale_x_continuous(breaks = seq(min(all_year$acc_year),max(all_year$acc_year), by = 1))+
+      scale_x_continuous(breaks = seq(min(period_all$acc_year),max(period_all$acc_year), by = 1))+
       theme_classic()+
       ggtitle("PL Trend")+
       xlab("AY") + ylab("Frequency")
